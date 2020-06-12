98de6bb14d845e01f03e5ff00547cade499c7cb7
Phillip Chen <phillipchen@Phillips-MacBook-Pro.local>
2020-06-12 13:33:56 +0800

    9th commit

--- assets/js/bundle.js
+++ assets/js/bundle.js
@@ -1,123 +1,101 @@
 // $('.preloader').animate({"opacity":"1"},500);
 
 $(window).on("load", function(){
-
-    if ($(document).find("title").text() !== "Yi-Tsung Chen") {
-      $('#project-overall-container').animate({"opacity":"1"},500);
-
-
-      if ($(document).find("title").text() === "Yi-Tsung - Project Qaimo") {
-
-        // Init data table
-        var table = $('#example').DataTable( {
-          "bAutoWidth": false,
-          pageLength : 5,
-          lengthMenu: [[5, 10, 20, 40], [5, 10, 20, 40]],
-          "scrollX": true
-        });
-
-        // Redraw table width on screen resizing
-        $(window).resize(function(){
-          table.columns.adjust().draw();
-        });
-
-        // flip card function
-        $('.chart-switch').on("click",function(){
-          flip();
-        });
-
-        function flip() {
-            $('.flip-card').toggleClass('flipped');
-        }
-
+  $('.header-container').animate({"opacity":"1"},500);
+  if ($(document).find("title").text() !== "Yi-Tsung Chen") {
+    $('#project-overall-container').animate({"opacity":"1"},500);
+    if ($(document).find("title").text() === "Yi-Tsung - Project Qaimo") {
+      // Init data table
+      var table = $('#example').DataTable( {
+        "bAutoWidth": false,
+        pageLength : 5,
+        lengthMenu: [[5, 10, 20, 40], [5, 10, 20, 40]],
+        "scrollX": true
+      });
+      // Redraw table width on screen resizing
+      $(window).resize(function(){
+        table.columns.adjust().draw();
+      });
+      // flip card function
+      $('.chart-switch').on("click",function(){
+        flip();
+      });
+      function flip() {
+          $('.flip-card').toggleClass('flipped');
       }
-
-      $('a.section-jumper-link').bind('click', function(e) {
-        e.preventDefault(); // prevent hard jump, the default behavior
-
-        var target = $(this).attr("href"); // Set the target as variable
-
-        // perform animated scrolling by getting top-position of target-element and set it as scroll target
-        $('html, body').stop().animate({
-            scrollTop: $(target).offset().top
-        }, 750, function() {
-            location.hash = target; //attach the hash (#jumptarget) to the pageurl
-        });
-        return false;
-      });
-
-
-      // Show & Hide section jumper-menu functions
-      $(".section-jumper-menu-switch").on("click",function(){
-        if ($(".section-jumper-wrapper").css("display") === "none") {
-          $("#project-header-section").animate({"opacity": "0.7"},450);
-          $("#project-content-section").animate({"opacity": "0.7"},450);
-          $("#footer-container").animate({"opacity": "0.7"},450);
-
-          $(".section-jumper-wrapper").fadeIn(450);
-          $('.jumper-text').fadeOut(150);
-        }else{
-          $("#project-header-section").animate({"opacity": "1"},450);
-          $("#project-content-section").animate({"opacity": "1"},450);
-          $("#footer-container").animate({"opacity": "1"},450);
-
-          $(".section-jumper-wrapper").fadeOut(150);
-          $('.jumper-text').fadeIn(450);
-        }
-        $(".section-jumper-menu-switch").toggleClass("jumper-switch-on");
-      });
-
-    }else {
-      $('.header-container').animate({"opacity":"1"},500);
-      // $('.navbar-container').animate({"opacity":"1"},200);
-      $('#home-project-section').animate({"opacity":"1"},500);
-
-
-      $('.projects-jumper').bind('click', function(e) {
-        e.preventDefault(); // prevent hard jump, the default behavior
-
-        var target = $(this).attr("href"); // Set the target as variable
-
-        // perform animated scrolling by getting top-position of target-element and set it as scroll target
-        $('html, body').stop().animate({
-            scrollTop: $(target).offset().top-30
-        }, 500, function() {
-            location.hash = target; //attach the hash (#jumptarget) to the pageurl
-        });
-        return false;
-      });
-
-
     }
 
-    // Mobile version menu animation when the device width is < 576 px
-    if (window.innerWidth < 576) {
-      $('.mobile-nav-switch-container').animate({"opacity":"1"},500);
-    }
-
-      // Mobile version menu animation when the device width resized until is < 576 px
-    $(window).resize(function(){
-      if (window.innerWidth < 576) {
-        $('.mobile-nav-switch-container').css("opacity","1");
-      }else{
-        $('.mobile-nav-switch-container').css("opacity","0");
-      }
+    $('a.section-jumper-link').bind('click', function(e) {
+      e.preventDefault(); // prevent hard jump, the default behavior
+      var target = $(this).attr("href"); // Set the target as variable
+      // perform animated scrolling by getting top-position of target-element and set it as scroll target
+      $('html, body').stop().animate({
+          scrollTop: $(target).offset().top
+      }, 750);
+      // , function() {
+          // location.hash = target; //attach the hash (#jumptarget) to the pageurl
+      // });
+      return false;
     });
 
+    // Show & Hide section jumper-menu functions
+    $(".section-jumper-menu-switch").on("click",function(){
+      if ($(".section-jumper-wrapper").css("display") === "none") {
+        $("#project-header-section").animate({"opacity": "0.7"},450);
+        $("#project-content-section").animate({"opacity": "0.7"},450);
+        $("#footer-container").animate({"opacity": "0.7"},450);
+        $(".section-jumper-wrapper").fadeIn(450);
+        $('.jumper-text').fadeOut(150);
+      }else{
+        $("#project-header-section").animate({"opacity": "1"},450);
+        $("#project-content-section").animate({"opacity": "1"},450);
+        $("#footer-container").animate({"opacity": "1"},450);
+        $(".section-jumper-wrapper").fadeOut(150);
+        $('.jumper-text').fadeIn(450);
+      }
+      $(".section-jumper-menu-switch").toggleClass("jumper-switch-on");
+    });
+  }else {
+    // $('.navbar-container').animate({"opacity":"1"},200);
+    $('#home-project-section').animate({"opacity":"1"},500);
+  }
 
-  // $('.preloader').fadeOut(750, function(){ });
-  // $('.header-container').animate({"opacity":"1"},700);
-  // $('.navbar-container').animate({"opacity":"1"},300);
-  // $('#home-project-section').animate({"opacity":"1"},700);
-  // $('.project-content-container').animate({"opacity":"1"},700);
+  if ($(document).find("title").text() === "Yi-Tsung Chen" || $(document).find("title").text() === "Yi-Tsung - About" || $(document).find("title").text() === "Yi-Tsung - Contact") {
+    $('.projects-jumper').bind('click', function(e) {
+      e.preventDefault(); // prevent hard jump, the default behavior
+
+      var target = $(this).attr("href"); // Set the target as variable
+
+      // perform animated scrolling by getting top-position of target-element and set it as scroll target
+      $('html, body').stop().animate({
+          scrollTop: $(target).offset().top
+      }, 375);
+
+      // , function() {
+          // location.hash = target; //attach the hash (#jumptarget) to the pageurl
+      // });
+      return false;
+    });
+  }
+  // Mobile version menu animation when the device width is < 576 px
+  if (window.innerWidth < 576) {
+    $('.mobile-nav-switch-container').animate({"opacity":"1"},500);
+  }
+
+    // Mobile version menu animation when the device width resized until is < 576 px
+  $(window).resize(function(){
+    if (window.innerWidth < 576) {
+      $('.mobile-nav-switch-container').css("opacity","1");
+    }else{
+      $('.mobile-nav-switch-container').css("opacity","0");
+    }
+  });
 
 });
-
 // Mobile navbar function
 function openNav() {
   document.getElementById("small-width-navbar").style.width = "100%";
 }
-
 function closeNav() {
   document.getElementById("small-width-navbar").style.width = "0%";
 }
