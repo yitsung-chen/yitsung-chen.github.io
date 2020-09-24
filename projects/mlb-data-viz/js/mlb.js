40369583d175d14a888cf72b9fb54d5c1e0abdb2
Phillip Chen <phillipchen@Phillips-MacBook-Pro.local>
2020-09-24 20:46:51 +0800

    18th commit

--- projects/mlb-data-viz/js/mlb.js
+++ projects/mlb-data-viz/js/mlb.js
@@ -609,8 +609,8 @@ function InfoDisplay(playerId, playerInfoData, preloader){
   var dashHeaderContainer = document.getElementById("dash-header");
   var playerImg = new Image();
   // var playerImgUrl = "https://securea.mlb.com/mlb/images/players/head_shot/" + playerId + ".jpg";
-  var pitcherUrl = "https://getdrawings.com/img/baseball-player-silhouette-vector-24.jpg";
-  var batterUrl = "https://getdrawings.com/img/baseball-player-silhouette-vector-4.jpg";
+  var pitcherUrl = "../imgs/pitcher.jpg";
+  var batterUrl = "../imgs/batter.jpg";
   var img = document.getElementById("player-pic");
   var returnBtn = document.getElementById("return-btn");
   var weekAvgEle = document.getElementById("w-avg");
